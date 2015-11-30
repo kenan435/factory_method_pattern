ef64e044e6194e252636d5fade0b4033507b8bf3
Kenan Karamehmedovic <kenan435@gmail.com>
2015-11-30 14:54:21 +0100

    First commit for the factory pattern

---
+++ factoryMethodPattern/src/main/java/com/kenan/App.java
@@ -0,0 +1,13 @@
+package com.kenan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
